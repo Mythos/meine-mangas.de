4c12a3d139d82f23a4c38aeb8797bbfb600f833e
Mythos <stefanschmid35@googlemail.com>
2022-11-21 18:22:14 +0100

    Add sm class to language picker

--- resources/views/livewire/language-switcher.blade.php
+++ resources/views/livewire/language-switcher.blade.php
@@ -1,5 +1,5 @@
 <div class="d-inline">
-    <select id="language" name="language" class="form-select @error('language') is-invalid @enderror" style="width: auto;" wire:model='language' required>
+    <select id="language" name="language" class="form-select form-select-sm @error('language') is-invalid @enderror" style="width: auto;" wire:model='language' required>
         <option value="de">{{ trans('languages.de') }}</option>
         <option value="en">{{ trans('languages.en') }}</option>
     </select>
